cd9147dc58766057ce3382a929f0fb35180d7c5c
Muhammad Hussein Ammari <xmha97@gmail.com>
2021-12-22 17:09:55 +0330

    Add speedtest package PARAMETERS

--- automatic/speedtest/tools/chocolateyInstall.ps1
+++ automatic/speedtest/tools/chocolateyInstall.ps1
@@ -4,9 +4,23 @@ $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $checksum       = '39a4f97512fda5033ccca28674b009e15056c780ea1c296265391aca4897a494'
 $checksumType   = 'sha256'
 
+### PARAMETERS
+If ($PackageParameters.InstallDir) {
+  $installDir = $PackageParameters.InstallDir
+} Else {
+  $installDir = "$toolsDir"
+}
+
 Install-ChocolateyZipPackage `
   -PackageName $packageName `
   -Url64bit "$url" `
-  -UnzipLocation "$toolsDir" `
+  -UnzipLocation "$installDir" `
   -Checksum64 $checksum `
   -ChecksumType64 $checksumType
+
+# Add StartMenu shortcut
+If ($PackageParameters.StartMenuShortcut) {
+  $programsPath = "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\"
+  $programsFilePath = Join-Path "$programsPath" "Speedtest CLI.lnk"
+  Install-ChocolateyShortcut -shortcutFilePath "$programsFilePath" -targetPath "$targetPath"
+}
