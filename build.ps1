4d94f637b8cdbd63c239e46ae88c2071b4659fe6
Muhammad Hussein Ammari <xmha97@gmail.com>
2021-12-23 01:52:17 +0330

    Updated Build Script

--- build.ps1
+++ build.ps1
@@ -1,10 +1,15 @@
 foreach ($d in Get-ChildItem(".\automatic") -Directory) {
     $rmPath = Join-Path $d.FullName 'README.md'
 	Remove-Item $rmPath -ErrorAction Ignore
-	$xmlPath = Join-Path $d.FullName ($d.Name + ".nuspec")
-	[XML]$xmlContent = Get-Content $xmlPath 
-	$rmContent = $xmlContent.package.metadata.description
+	$specPath = Join-Path $d.FullName ($d.Name + ".nuspec")	
+	[XML]$specContent = Get-Content $specPath 
+	$pkgID = $specContent.package.metadata.id
+	$pkgV = $specContent.package.metadata.version
+	choco pack $specPath
+	$pkgName = "$pkgID.$pkgV.nupkg"
+	choco push $pkgName --source https://push.chocolatey.org/
+	Remove-Item $pkgName -ErrorAction Ignore
+	$rmContent = $specContent.package.metadata.description
 	New-Item $rmPath
 	Set-Content $rmPath $rmContent
 }
-pause
