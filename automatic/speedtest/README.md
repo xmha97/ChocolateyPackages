a2e676d93c4ed5191a7f065afb733d01772ae424
Muhammad Hussein Ammari <xmha97@gmail.com>
2021-12-22 23:20:24 +0330

    Fix Parameters

--- automatic/speedtest/README.md
+++ automatic/speedtest/README.md
@@ -5,13 +5,13 @@ Use Speedtest® for a quick, easy, one-tap connection internet speed test—accu
 ## Package Parameters
 Command-line options for installer configuration:
 - `/InstallDir:PATH`
-- `/StartMenuShortcut` Do not create Start Menu Shortcut
+- `/StartMenuShortcut` Create the Start Menu Shortcut
 
 ### Examples
 - Install + start menu shortcut:
-`choco install lossless-cut --params "/StartMenuShortcut"`
+`choco install speedtest --params "/StartMenuShortcut"`
 - Portable mode:
-`choco install lossless-cut --params "/InstallDir:C:\your\install\path"`
+`choco install speedtest --params "/InstallDir:C:\speedtest"`
 
 ### Default Parameters
 By default, **installation** of this package:
--- automatic/speedtest/tools/chocolateyInstall.ps1
+++ automatic/speedtest/tools/chocolateyInstall.ps1
@@ -1,25 +1,27 @@
-$packageName    = 'speedtest'
-$url            = 'https://install.speedtest.net/app/cli/ookla-speedtest-1.1.1-win64.zip'
-$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$checksum       = '39a4f97512fda5033ccca28674b009e15056c780ea1c296265391aca4897a494'
-$checksumType   = 'sha256'
+$ErrorActionPreference = 'Stop';
+$toolsDir   =	 "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$PackageParameters = Get-PackageParameters
 
 ### PARAMETERS
 If ($PackageParameters.InstallDir) {
   $installDir = $PackageParameters.InstallDir
-} Else {
+} else {
   $installDir = "$toolsDir"
 }
 
-Install-ChocolateyZipPackage `
-  -PackageName $packageName `
-  -Url64bit "$url" `
-  -UnzipLocation "$installDir" `
-  -Checksum64 $checksum `
-  -ChecksumType64 $checksumType
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = "$installDir"
+  url            = 'https://install.speedtest.net/app/cli/ookla-speedtest-1.1.1-win64.zip'
+  checksum       = '39a4f97512fda5033ccca28674b009e15056c780ea1c296265391aca4897a494'
+  checksumType   = 'sha256'
+}
+Install-ChocolateyZipPackage @packageArgs
+
+$targetPath = Join-Path "$installDir" "LosslessCut.exe"
 
 # Add StartMenu shortcut
-If ($PackageParameters.StartMenuShortcut) {
+If ( $PackageParameters.StartMenuShortcut ) {
   $programsPath = "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\"
   $programsFilePath = Join-Path "$programsPath" "Speedtest CLI.lnk"
   Install-ChocolateyShortcut -shortcutFilePath "$programsFilePath" -targetPath "$targetPath"
