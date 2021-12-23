378c55ec7bb5be5d7cd2eba84c1932eba905a264
Muhammad Hussein Ammari <xmha97@gmail.com>
2021-12-23 12:28:54 +0330

    Updated Build Script

--- build.ps1
+++ build.ps1
@@ -1,15 +1,23 @@
-foreach ($d in Get-ChildItem(".\automatic") -Directory) {
-    $rmPath = Join-Path $d.FullName 'README.md'
-	Remove-Item $rmPath -ErrorAction Ignore
+$pkgDir = '.\automatic'
+$armPath = Join-Path $pkgDir 'README.md'
+Remove-Item $armPath -ErrorAction Ignore
+$armContent = '#Packages'
+foreach ($d in Get-ChildItem($pkgDir) -Directory) {
+    $prmPath = Join-Path $d.FullName 'README.md'
+	Remove-Item $prmPath -ErrorAction Ignore
 	$specPath = Join-Path $d.FullName ($d.Name + ".nuspec")	
 	[XML]$specContent = Get-Content $specPath 
 	$pkgID = $specContent.package.metadata.id
 	$pkgV = $specContent.package.metadata.version
+	$pkgT = $specContent.package.metadata.title
 	choco pack $specPath
 	$pkgName = "$pkgID.$pkgV.nupkg"
 	choco push $pkgName --source https://push.chocolatey.org/
 	Remove-Item $pkgName -ErrorAction Ignore
-	$rmContent = $specContent.package.metadata.description
-	New-Item $rmPath
-	Set-Content $rmPath $rmContent
+	$prmContent = $specContent.package.metadata.description
+	New-Item $prmPath
+	Set-Content $prmPath $prmContent
+	$armContent = "$armContent`n[$pkgT](https://github.com/xmha97/chocolatey-coreteampackages/tree/main/automatic/$d)"
 }
+New-Item $armPath
+Set-Content $armPath $armContent
